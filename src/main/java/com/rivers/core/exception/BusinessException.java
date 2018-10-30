e06780770fd516b61cd5039e9241e63f4e0ccccc
wangyichuan <kingjames6236@163.com>
2018-10-30 10:47:27 +0800

    update

---
+++ src/main/java/com/rivers/core/exception/BusinessException.java
@@ -0,0 +1,27 @@
+package com.rivers.core.exception;
+
+public class BusinessException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+    private String errcode = null;
+
+    public BusinessException(String errorCode) {
+        this.errcode = errorCode;
+    }
+
+    public BusinessException(String errorCode, String msg) {
+        super(msg);
+        this.errcode = errorCode;
+    }
+
+    public BusinessException(String errorCode, Throwable msg) {
+        super(msg);
+        this.errcode = errorCode;
+    }
+
+    public String getErrcode() {
+        return this.errcode;
+    }
+
+
+}
---
+++ src/main/java/com/rivers/core/util/ExceptionUtil.java
@@ -0,0 +1,20 @@
+package com.rivers.core.util;
+
+import com.rivers.core.exception.BusinessException;
+import org.springframework.util.Assert;
+
+public class ExceptionUtil extends Assert {
+
+        public static void  throwBusinessException(String errorCode) {
+            throw new BusinessException(errorCode);
+        }
+
+    public static void  throwBusinessException(String errorCode,String msg) {
+        throw new BusinessException(errorCode, msg);
+    }
+
+        public static void  throwBusinessException(String errorCode,Throwable e) {
+            throw new BusinessException(errorCode, e);
+        }
+
+}
