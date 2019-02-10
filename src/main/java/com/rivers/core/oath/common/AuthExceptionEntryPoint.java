d85957e290cf36026cd85c497de06b49c80e3d40
riversking <kingjames6236@163.com>
2019-02-10 23:14:45 +0800

    update

--- src/main/java/com/rivers/core/oath/common/AuthExceptionEntryPoint.java
+++ src/main/java/com/rivers/core/oath/common/AuthExceptionEntryPoint.java
@@ -11,6 +11,9 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * @author riversking
+ */
 public class AuthExceptionEntryPoint implements AuthenticationEntryPoint {
     /**
      * token错误时进入到这里
